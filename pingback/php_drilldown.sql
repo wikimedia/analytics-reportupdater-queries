303c65f0b553975669d0783e4b1a0d43488996f2
Cindy Cicalese <ccicalese@wikimedia.org>
2018-03-12 14:34:05 +0000

    Added queries of PHP version by MediaWiki version.
    Change-Id: Iee081de81da538d32c7b12f264552d912efa7c05

---
+++ pingback/php_drilldown.sql
@@ -0,0 +1,25 @@
+SELECT
+	DATE('{from_timestamp}') AS date,
+	SUM(event_PHP LIKE '5.5%') AS '5.5',
+	SUM(event_PHP LIKE '5.6%') AS '5.6',
+	SUM(event_PHP LIKE '7.0%') AS '7.0',
+	SUM(event_PHP LIKE '7.1%') AS '7.1',
+	SUM(event_PHP LIKE '7.2%') AS '7.2',
+	SUM(event_PHP NOT LIKE '5.5%' AND
+		event_PHP NOT LIKE '5.6%' AND
+		event_PHP NOT LIKE '7.0%' AND
+		event_PHP NOT LIKE '7.1%' AND
+		event_PHP NOT LIKE '7.2%') AS 'Other'
+FROM MediaWikiPingback_15781718
+JOIN (
+	SELECT
+		MAX(id) AS id
+	FROM MediaWikiPingback_15781718
+	WHERE
+		timestamp < '{to_timestamp}'
+	GROUP BY wiki
+) AS latest
+USING (id)
+WHERE
+	event_PHP != '' AND
+	event_MediaWiki LIKE CONCAT('{MediaWiki_Version}', '%')
