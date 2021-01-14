f1484b39d18426f92b8fcaa4ed434fddafec5590
Adam Wight <adam.wight@wikimedia.de>
2021-01-14 17:31:53 +0100

    Segment CodeMirror metrics by user edit count
    Includes safe migration and backfill logic, which can be
    removed later.
    
    Bug: T273471
    Change-Id: I2c5ef8f4c36d3d573881ad5045be01ca12379a52

--- codemirror/sql/users_codemirror_and_wikitext.sql
+++ codemirror/sql/users_codemirror_and_wikitext.sql
@@ -10,16 +10,31 @@ WITH
             -- These default to 0, only 1 is recorded as a row.
             MAX(up_property = 'usecodemirror' AND up_value = '1') AS syntax_enabled,
             MAX(up_property = 'visualeditor-newwikitext' AND up_value = '1') AS 2017_enabled,
-            MAX(up_property = 'gadget-wikEd' AND up_value = '1') AS wiked_enabled
+            MAX(up_property = 'gadget-wikEd' AND up_value = '1') AS wiked_enabled,
             -- 'usebetatoolbar' defaults to 1
-            ! MAX(up_property = 'usebetatoolbar' AND up_value = '0') AS 2010_enabled,
+            ! MAX(up_property = 'usebetatoolbar' AND up_value = '0') AS 2010_enabled
         FROM user_properties
         GROUP BY user_id
         -- Suppress the default of ordering by the `group by` column.
         ORDER BY NULL
+    ),
+    user_buckets AS (
+        SELECT
+            editor_preference.user_id,
+            CASE
+                WHEN user_editcount >= 1000 THEN '1000+ edits'
+                WHEN user_editcount >= 100 THEN '100-999 edits'
+                WHEN user_editcount >= 5 THEN '5-99 edits'
+                WHEN user_editcount >= 1 THEN '1-4 edits'
+                ELSE '0 edits'
+            END as edit_count_bucket
+        FROM editor_preference
+        JOIN user
+            ON editor_preference.user_id = user.user_id
     )
     SELECT
         DATE('{from_timestamp}') AS `date`,
+        edit_count_bucket,
         SUM(wiked_enabled) AS users_gadget_wiked,
 
         -- Apply precedence logic to calculate the expected editor experience:
@@ -33,4 +48,8 @@ WITH
         -- How many in each editor have highlighting enabled?
         SUM(2010_enabled && ! 2017_enabled && ! wiked_enabled && syntax_enabled) AS users_wikitext_2010_and_codemirror,
         SUM(2017_enabled && syntax_enabled) AS users_wikitext_2017_and_codemirror
-    FROM editor_preference;
+    FROM editor_preference
+    JOIN user_buckets
+        ON editor_preference.user_id = user_buckets.user_id
+    GROUP BY
+        edit_count_bucket;
