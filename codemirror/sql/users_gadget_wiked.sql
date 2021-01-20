63da4f12f7c69874ee2c56fbe2c95e2e199d984e
Svantje Lilienthal <svantje.lilienthal@wikimedia.de>
2021-01-20 16:21:39 +0100

    Added editor type preferences
    We want to produce totals for any missing normalizations.
    To normalize the CodeMirror usage numbers we need the total
    number of users for each editor type (2010 and 2017).
    
    * We combined 2010, 2017 and wikEd queries in one.
    * We removed extension CodeEditor because it does not conflict.
    
    Bug: T271902
    Change-Id: I9759381ce2130cbf8f22eb36bd896b63c84547df

--- codemirror/sql/users_gadget_wiked.sql
+++
@@ -1,8 +0,0 @@
-SELECT
-    DATE('{from_timestamp}') AS DATE,
-    COUNT(*) AS users_gadget_wiked
-FROM
-    {wiki_db}.user_properties
-WHERE
-    up_property = 'gadget-wikEd'
-    AND up_value = 1;
