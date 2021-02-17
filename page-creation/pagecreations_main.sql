b2ede546f8ec03f2557232fda42c58901b19234c
Dan Andreescu <dandreescu@wikimedia.org>
2021-02-17 17:03:20 -0500

    Remove disabled jobs from reportupdater
    These jobs are not scheduled via puppet, if anyone needs them they'll be
    here in the history.
    
    Change-Id: I0c14ee0fe85ff589a5973ba33bc8f398506d9bf6

--- page-creation/pagecreations_main.sql
+++
@@ -1,7 +0,0 @@
-SELECT DATE('{from_timestamp}') AS `date`,
-COUNT(*) AS {wiki_db}
-FROM mediawiki_page_create_3
-WHERE `database`='{wiki_db}'
-AND page_namespace = 0
-AND rev_timestamp >= STR_TO_DATE('{from_timestamp}', '%Y%m%d%H%i%s')
-AND rev_timestamp < STR_TO_DATE('{to_timestamp}', '%Y%m%d%H%i%s')
